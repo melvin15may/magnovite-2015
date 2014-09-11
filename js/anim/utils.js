78a52b9acab6278362060384e7f62415a358b032
Ahmed Azaan <azaan@outlook.com>
2014-09-11 09:15:03 +0530

    add captureMouse function to utils

---
+++ js/anim/utils.js
@@ -0,0 +1,33 @@
+var anim = anim || {};
+
+(function() {
+
+    anim.util = {};
+
+    anim.util.captureMouse = function(el) {
+        var mouse = {x: 0, y:0};
+
+        el.addEventListener('mousemove', function(e) {
+            var x, y;
+
+            if (e.pageX || e.pageY) {
+                x = e.pageX;
+                y = e.pageY;
+            } else {
+                x = e.clientX + document.body.scrollLeft +
+                        document.documentElement.scrollLeft;
+                y = e.clientY + document.body.scrollTop +
+                        document.documentElement.scrollTop;
+            }
+
+            x -= el.offsetLeft;
+            y -= el.offsetTop;
+
+            mouse.x = x;
+            mouse.y = y;
+        }, false);
+
+        return mouse;
+    }
+
+})();
